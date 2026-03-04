d2ccdb64991ad39518ddd5abf5be553fad70a3e2
LouieLemon-lab <anjunabear28@gmail.com>
2026-03-04 10:18:56 +0000

    Update HyperionMod.java

--- src/main/java/com/hyperion/mod/HyperionMod.java
+++ src/main/java/com/hyperion/mod/HyperionMod.java
@@ -1,16 +1,28 @@
 package com.hyperion.mod;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.item.crafting.SimpleRecipeSerializer;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import org.slf4j.Logger;
 
 @Mod("hyperion")
 public class HyperionMod {
     public static final Logger LOGGER = LogUtils.getLogger();
 
+    public static final DeferredRegister<RecipeSerializer<?>> RECIPE_SERIALIZERS =
+        DeferredRegister.create(Registries.RECIPE_SERIALIZER, "hyperion");
+
+    public static final DeferredHolder<RecipeSerializer<?>, SimpleRecipeSerializer<HyperionRecipe>> HYPERION_RECIPE_SERIALIZER =
+        RECIPE_SERIALIZERS.register("hyperion_craft", () -> new SimpleRecipeSerializer<>(HyperionRecipe::new));
+
     public HyperionMod(IEventBus modEventBus) {
+        RECIPE_SERIALIZERS.register(modEventBus);
         NeoForge.EVENT_BUS.register(HyperionEvents.class);
         NeoForge.EVENT_BUS.register(HyperionCommand.class);
         LOGGER.info("Hyperion mod loaded!");
