8b8ec3434d5bf32943048ec682e687aa73896e30
LouieLemon-lab <anjunabear28@gmail.com>
2026-03-04 13:46:01 +0000

    Update HyperionMod.java

--- src/main/java/com/hyperion/mod/HyperionMod.java
+++ src/main/java/com/hyperion/mod/HyperionMod.java
@@ -4,6 +4,7 @@ import com.mojang.logging.LogUtils;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.common.NeoForge;
@@ -15,13 +16,19 @@ import org.slf4j.Logger;
 public class HyperionMod {
     public static final Logger LOGGER = LogUtils.getLogger();
 
+    public static final DeferredRegister<Item> ITEMS =
+        DeferredRegister.create(Registries.ITEM, "hyperion");
+
+    public static final DeferredHolder<Item, HyperionItem> HYPERION_ITEM =
+        ITEMS.register("hyperion_sword", HyperionItem::new);
+
     public static final DeferredRegister<CreativeModeTab> CREATIVE_TABS =
         DeferredRegister.create(Registries.CREATIVE_MODE_TAB, "hyperion");
 
     public static final DeferredHolder<CreativeModeTab, CreativeModeTab> HYPERION_TAB =
         CREATIVE_TABS.register("hyperion_tab", () -> CreativeModeTab.builder()
             .title(Component.literal("Hyperion"))
-            .icon(HyperionCommand::buildHyperionStack)
+            .icon(() -> HyperionCommand.buildHyperionStack())
             .displayItems((params, output) -> {
                 output.accept(HyperionCommand.buildHyperionStack());
             })
@@ -29,6 +36,7 @@ public class HyperionMod {
         );
 
     public HyperionMod(IEventBus modEventBus) {
+        ITEMS.register(modEventBus);
         CREATIVE_TABS.register(modEventBus);
         NeoForge.EVENT_BUS.register(HyperionEvents.class);
         NeoForge.EVENT_BUS.register(HyperionCommand.class);
